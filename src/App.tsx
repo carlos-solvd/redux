4790848396e3517a76c6940af582dc14d238592a
carlos-solvd <cealvesdegodoi@solvd.com>
2024-01-11 15:57:05 -0300

    feat: created action on Redux

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Provider as ReduxProvider } from 'react-redux'
 
 import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
-import { store } from './store/intex';
+import { store } from './store';
 
 export function App() {
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,10 @@
-import { useSelector } from 'react-redux'
+import { useAppSelector } from '../store'
 
 export function TodoList() {
-    const todos = useSelector(store => {
+    const todos = useAppSelector(store => {
         return store.todo
     })
 
-    console.log(todos)
     return (
         <ul>
             {todos.map(todo => <li key={todo}>{todo}</li>)}
