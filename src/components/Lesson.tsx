41d4d5abc276b3b4372ae95bfb9f80ec4331a588
carlos-solvd <cealvesdegodoi@solvd.com>
2024-01-11 18:06:44 -0300

    feat: created reducer of player

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -5,7 +5,7 @@ interface LessonProps {
     duration: string
 }
 
-export function Lesson( { title, duration }: LessonProps) {
+export function Lesson({ title, duration }: LessonProps) {
     return (
         <button className=' flex items-center gap-3 text-sm text-zinc-400'>
             <Video className=' w-4 h-4 text-zinc-500' />
