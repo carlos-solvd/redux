5185e9d9b1c92b946ce644058476bd9cf0522b08
carlos-solvd <cealvesdegodoi@solvd.com>
2024-01-12 15:45:31 -0300

    feat: loading data of courses from API

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
-import { useCurrentLesson } from "../store/slices/player"
+import { useCurrentLesson } from "../store/slices/player";
 
 export function Header() {
-    const {currentModule, currentLesson} = useCurrentLesson()
-    
+    const { currentModule, currentLesson } = useCurrentLesson()
+
+    if (!currentModule || !currentLesson) {
+        return null
+    }
+
     return (
         <div className="flex flex-col gap-1">
             <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
-            <span className="text-sm text-zinc-400">{currentModule.title}</span>
+            <span className="text-sm text-zinc-400">Módulo "{currentModule.title}"</span>
         </div>
-    )
+    );
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,18 +1,19 @@
 import ReactPlayer from 'react-player'
-import { useDispatch } from 'react-redux'
-import { next, useCurrentLesson } from '../store/slices/player'
+import { useDispatch } from "react-redux";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
     const dispatch = useDispatch()
-
     const { currentLesson } = useCurrentLesson()
 
     function handlePlayNext() {
         dispatch(next())
     }
 
+    if (!currentLesson) return null;
+
     return (
-        <div className=' w-full bg-zinc-950 aspect-video'>
+        <div className="w-full bg-zinc-950 aspect-video">
             <ReactPlayer
                 width="100%"
                 height="100%"
@@ -22,5 +23,5 @@ export function Video() {
                 url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
             />
         </div>
-    )
+    );
 }
