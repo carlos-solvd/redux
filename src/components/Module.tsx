94311bf2a11a1bda5aa1e7ec35e292053439c25f
carlos-solvd <cealvesdegodoi@solvd.com>
2024-01-12 14:08:35 -0300

    feat: show active lesson with css properties

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -15,9 +15,15 @@ interface ModuleProps {
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
     const dispatch = useDispatch()
 
-     const lessons = useAppSelector((state) => {
+    const { currentModuleIndex, currentLessonIndex } = useAppSelector(state =>{
+        const { currentModuleIndex, currentLessonIndex } = state.player
+
+        return { currentModuleIndex, currentLessonIndex }
+    })
+
+    const lessons = useAppSelector((state) => {
         return state.player.course.modules[moduleIndex].lessons
-     })
+    })
 
     return (
         <Collapsible.Root className="group">
@@ -35,8 +41,16 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
             <Collapsible.Content>
                 <nav className=' relative flex flex-col gap-4 p-6'>
                     {lessons.map((lesson, lessonIndex) => {
+                        const isCurrent = currentModuleIndex === moduleIndex && currentLessonIndex === lessonIndex
+
                         return (
-                            <Lesson key={lesson.id} title={lesson.title} duration={lesson.duration} onPlay={() => {dispatch(play([moduleIndex, lessonIndex]))}} />
+                            <Lesson
+                                key={lesson.id}
+                                title={lesson.title}
+                                duration={lesson.duration}
+                                onPlay={() => { dispatch(play([moduleIndex, lessonIndex])) }}
+                                isCurrent={ isCurrent }
+                            />
                         )
                     })}
                 </nav>
